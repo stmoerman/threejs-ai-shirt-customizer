e84d4e2658719ed7c57651fac294d940ca0be52b
Stephan Moerman <stephan.moerman@viewnary.com>
2023-08-23 13:24:31 +0200

    Uploading project to GitHub

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+
+import dalleRoutes from "./routes/dalle.routes.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+
+app.use("/api/v1/dalle", dalleRoutes);
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello from DALL.E" });
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
